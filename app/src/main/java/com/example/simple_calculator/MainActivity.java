b372d324f806f009c158ccf2f90aa2d152e1bbea
Abhishek Kumar <abhishekkumar15062002@gmail.com>
2024-02-26 22:07:39 +0530

    This is First Version

--- app/src/main/java/com/example/simple_calculator/MainActivity.java
+++ app/src/main/java/com/example/simple_calculator/MainActivity.java
@@ -10,121 +10,126 @@ import android.widget.TextView;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
 
+import javax.xml.xpath.XPathExpressionException;
+
 public class MainActivity extends AppCompatActivity {
 
     ImageView btn_0, btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9;
-    ImageView btn_dot, btn_equal, btn_plus, btn_minus, btn_multiply, btn_divide, btn_mode, btn_ac, btn_delete;
+    ImageView btn_dot, btn_equal, btn_plus, btn_minus,
+
+    btn_multiply, btn_divide, btn_mode, btn_ac, btn_delete;
 
     TextView textInput, textOutput;
 
     String data;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textInput=findViewById(R.id.textInput);
-        textOutput=findViewById(R.id.textOutput);
+        textInput = findViewById(R.id.textInput);
+        textOutput = findViewById(R.id.textOutput);
 
-        btn_0=findViewById(R.id.btn_0);
-        btn_1=findViewById(R.id.btn_1);
-        btn_2=findViewById(R.id.btn_2);
-        btn_3=findViewById(R.id.btn_3);
-        btn_4=findViewById(R.id.btn_4);
-        btn_5=findViewById(R.id.btn_5);
-        btn_6=findViewById(R.id.btn_6);
-        btn_7=findViewById(R.id.btn_7);
-        btn_8=findViewById(R.id.btn_8);
-        btn_9=findViewById(R.id.btn_9);
+        btn_0 = findViewById(R.id.btn_0);
+        btn_1 = findViewById(R.id.btn_1);
+        btn_2 = findViewById(R.id.btn_2);
+        btn_3 = findViewById(R.id.btn_3);
+        btn_4 = findViewById(R.id.btn_4);
+        btn_5 = findViewById(R.id.btn_5);
+        btn_6 = findViewById(R.id.btn_6);
+        btn_7 = findViewById(R.id.btn_7);
+        btn_8 = findViewById(R.id.btn_8);
+        btn_9 = findViewById(R.id.btn_9);
 
-        btn_dot=findViewById(R.id.btn_dot);
-        btn_equal=findViewById(R.id.btn_equal);
-        btn_plus=findViewById(R.id.btn_plus);
-        btn_minus=findViewById(R.id.btn_minus);
-        btn_multiply=findViewById(R.id.btn_multiply);
-        btn_divide=findViewById(R.id.btn_divide);
-        btn_mode=findViewById(R.id.btn_mode);
-        btn_ac=findViewById(R.id.btn_ac);
-        btn_delete=findViewById(R.id.btn_delete);
+        btn_dot = findViewById(R.id.btn_dot);
+        btn_equal = findViewById(R.id.btn_equal);
+        btn_plus = findViewById(R.id.btn_plus);
+        btn_minus = findViewById(R.id.btn_minus);
+        btn_multiply = findViewById(R.id.btn_multiply);
+        btn_divide = findViewById(R.id.btn_divide);
+        btn_mode = findViewById(R.id.btn_mode);
+        btn_ac = findViewById(R.id.btn_ac);
+        btn_delete = findViewById(R.id.btn_delete);
 
         btn_0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=textInput.getText().toString();
-                textInput.setText(data+"0");
+                data = textInput.getText().toString();
+                textInput.setText(data + "0");
             }
         });
 
         btn_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=textInput.getText().toString();
-                textInput.setText(data+"1");
+                data = textInput.getText().toString();
+                textInput.setText(data + "1");
             }
         });
 
         btn_2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=textInput.getText().toString();
-                textInput.setText(data+"2");
+                data = textInput.getText().toString();
+                textInput.setText(data + "2");
             }
         });
 
         btn_3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=textInput.getText().toString();
-                textInput.setText(data+"3");
+                data = textInput.getText().toString();
+                textInput.setText(data + "3");
             }
         });
 
         btn_4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=textInput.getText().toString();
-                textInput.setText(data+"4");
+                data = textInput.getText().toString();
+                textInput.setText(data + "4");
             }
         });
 
         btn_5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=textInput.getText().toString();
-                textInput.setText(data+"5");
+                data = textInput.getText().toString();
+                textInput.setText(data + "5");
             }
         });
 
         btn_6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=textInput.getText().toString();
-                textInput.setText(data+"6");
+                data = textInput.getText().toString();
+                textInput.setText(data + "6");
             }
         });
 
         btn_7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=textInput.getText().toString();
-                textInput.setText(data+"7");
+                data = textInput.getText().toString();
+                textInput.setText(data + "7");
             }
         });
 
         btn_8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=textInput.getText().toString();
-                textInput.setText(data+"8");
+                data = textInput.getText().toString();
+                textInput.setText(data + "8");
             }
         });
 
         btn_9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=textInput.getText().toString();
-                textInput.setText(data+"9");
+                data = textInput.getText().toString();
+                textInput.setText(data + "9");
             }
         });
 
@@ -139,84 +144,88 @@ public class MainActivity extends AppCompatActivity {
         btn_dot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=textInput.getText().toString();
-                textInput.setText(data+".");
+                data = textInput.getText().toString();
+                textInput.setText(data + ".");
             }
         });
 
         btn_plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=textInput.getText().toString();
-                textInput.setText(data+"+");
+                data = textInput.getText().toString();
+                textInput.setText(data + "+");
             }
         });
 
         btn_minus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=textInput.getText().toString();
-                textInput.setText(data+"-");
+                data = textInput.getText().toString();
+                textInput.setText(data + "-");
             }
         });
 
         btn_multiply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=textInput.getText().toString();
-                textInput.setText(data+"×");
+                data = textInput.getText().toString();
+                textInput.setText(data + "×");
             }
         });
 
         btn_divide.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=textInput.getText().toString();
-                textInput.setText(data+"÷");
+                data = textInput.getText().toString();
+                textInput.setText(data + "÷");
             }
         });
 
         btn_mode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=textInput.getText().toString();
-                textInput.setText(data+"%");
+                data = textInput.getText().toString();
+                textInput.setText(data + "%");
             }
         });
 
         btn_equal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=textInput.getText().toString();
+                data = textInput.getText().toString();
 
-                data=data.replaceAll("×","*");
-                data=data.replaceAll("%","/100");
-                data=data.replaceAll("÷","/");
+                data = data.replaceAll("×", "*");
+                data = data.replaceAll("%", "/100");
+                data = data.replaceAll("÷", "/");
 
-                Context rhino=Context.enter();
+                Context rhino = Context.enter();
                 rhino.setOptimizationLevel(-1);
 
-                String finalresult="";
+                String finalresult = "";
 
-                Scriptable scriptable=rhino.initStandardObjects();
-                finalresult=rhino.evaluateString(scriptable,data,"Javascript",1,null).toString();
+                Scriptable scriptable = rhino.initStandardObjects();
+                finalresult = rhino.evaluateString(scriptable, data, "Javascript", 1, null).toString();
 
                 textOutput.setText(finalresult);
             }
         });
 
 
-
-
         btn_delete.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-              if(data.length()>0){  data = data.substring(0, data.length()-1);
-                     textInput.setText(data);}
-              else{
-                  textInput.setText("");
-              }
+                try {
+                    if (data.length() > 0) {
+                        // If there is at least one character, remove the last one
+                        data = data.substring(0, data.length()-1);
+                        textInput.setText(data);
+                    } else {
+                        // If there are no characters left, do nothing or handle it as needed
+                        textInput.setText("");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
 
